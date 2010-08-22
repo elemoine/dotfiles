7c4000bed016def69821ad01082530cafa5a836b
Éric Lemoine <eric.lemoine@gmail.com>
2010-08-22 15:41:56 +0200

    install.py - use rmtree for removing directories

--- install.py
+++ install.py
@@ -6,7 +6,9 @@ exclude = [ '*.sw*', '.git', 'install.*', '.gitmodules' ]
 for f in os.listdir('.'):
     if not any(fnmatch.fnmatch(f, p) for p in exclude):
         path = os.path.join(os.path.expanduser('~'), f)
-        if os.path.isfile(path) or os.path.isdir(path):
+        if os.path.isfile(path):
             os.unlink(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
         os.symlink(os.path.abspath(f), path)
         print 'create link for %s' % (path)
