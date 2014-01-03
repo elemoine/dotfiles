54453d2b7e568c52a5febefd4abe7cd81ff32d1e
Éric Lemoine <eric.lemoine@gmail.com>
2014-01-03 21:32:02 +0100

    [install-dotfiles.py] handle .config correctly

--- install-dotfiles.py
+++ install-dotfiles.py
@@ -4,14 +4,24 @@ import fnmatch
 import shutil
 
 
-exclude = ['*.sw*', '.git', '*.un~', 'install-dotfiles.py', '.gitmodules', 'Makefile',
-           'build', 'requirements.txt', 'virtualenv-burrito']
+def create_symlink(dotname, directory=''):
+    path = os.path.join(os.path.expanduser('~'), directory, dotname)
+    if os.path.isfile(path) or os.path.islink(path):
+        os.unlink(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+    os.symlink(os.path.abspath(os.path.join(directory, dotname)), path)
+    print 'create link for %s' % (path)
+
+
+exclude = ['*.sw*', '.git', '*.un~', 'install-dotfiles.py',
+    '.gitmodules', 'Makefile', 'build', 'requirements.txt',
+    'virtualenv-burrito', '.config']
+
 for f in os.listdir('.'):
     if not any(fnmatch.fnmatch(f, p) for p in exclude):
-        path = os.path.join(os.path.expanduser('~'), f)
-        if os.path.isfile(path) or os.path.islink(path):
-            os.unlink(path)
-        elif os.path.isdir(path):
-            shutil.rmtree(path)
-        os.symlink(os.path.abspath(f), path)
-        print 'create link for %s' % (path)
+        create_symlink(f)
+
+for f in os.listdir('.config'):
+    if not any(fnmatch.fnmatch(f, p) for p in exclude):
+        create_symlink(f, '.config')
