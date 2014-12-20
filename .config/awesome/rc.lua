82e6b1b1c53d97f2a74e67876956bc16a57ae609
Éric Lemoine <eric.lemoine@gmail.com>
2014-12-20 23:09:15 +0100

    [awesome] Use dmenu_run

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -277,28 +277,29 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey            }, "F12",    function () awful.util.spawn ("gksu halt") end),
     -- Run or raise applications with dmenu (https://awesome.naquadah.org/wiki/Using_dmenu)
     -- Requires dmenu (apt-get install dmenu)
-    awful.key({ modkey            }, "r",
-        function ()
-            local f_reader = io.popen( "dmenu_path | dmenu -b -nb '".. beautiful.bg_normal .."' -nf '".. beautiful.fg_normal .."' -sb '#955'")
-            local command = assert(f_reader:read('*a'))
-            f_reader:close()
-            if command == "" then return end
+    awful.key({modkey             }, "r", function() awful.util.spawn( "dmenu_run" ) end)
+    --awful.key({ modkey            }, "r",
+        --function ()
+            --local f_reader = io.popen( "dmenu_path | dmenu -b -nb '".. beautiful.bg_normal .."' -nf '".. beautiful.fg_normal .."' -sb '#955'")
+            --local command = assert(f_reader:read('*a'))
+            --f_reader:close()
+            --if command == "" then return end
 
-            -- Check throught the clients if the class match the command
-            local lower_command=string.lower(command)
-            for k, c in pairs(client.get()) do
-                local class=string.lower(c.class)
-                if string.match(class, lower_command) then
-                    for i, v in ipairs(c:tags()) do
-                        awful.tag.viewonly(v)
-                        c:raise()
-                        c.minimized = false
-                        return
-                    end
-                end
-            end
-            awful.util.spawn(command)
-    end)
+            ---- Check throught the clients if the class match the command
+            --local lower_command=string.lower(command)
+            --for k, c in pairs(client.get()) do
+                --local class=string.lower(c.class)
+                --if string.match(class, lower_command) then
+                    --for i, v in ipairs(c:tags()) do
+                        --awful.tag.viewonly(v)
+                        --c:raise()
+                        --c.minimized = false
+                        --return
+                    --end
+                --end
+            --end
+            --awful.util.spawn(command)
+    --end)
 
 )
 
