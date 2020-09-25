a20d00053b5eb816349911cc40a7c31954070c7a
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-09-25 17:59:39 +0200

    Blackify install-dotfiles.py

--- install-dotfiles.py
+++ install-dotfiles.py
@@ -4,8 +4,8 @@ import fnmatch
 import shutil
 
 
-def create_symlink(dotname, directory=''):
-    directory_path = os.path.join(os.path.expanduser('~'), directory)
+def create_symlink(dotname, directory=""):
+    directory_path = os.path.join(os.path.expanduser("~"), directory)
     if not os.path.isdir(directory_path):
         os.mkdir(directory_path)
     path = os.path.join(directory_path, dotname)
@@ -14,19 +14,32 @@ def create_symlink(dotname, directory=''):
     elif os.path.isdir(path):
         shutil.rmtree(path)
     os.symlink(os.path.abspath(os.path.join(directory, dotname)), path)
-    print('create link for %s' % path)
+    print("create link for %s" % path)
 
 
-exclude = ['*.sw*', '.git', '*.un~', 'install-dotfiles.py',
-    '.gitmodules', 'Makefile', 'build', 'requirements.txt',
-    '.config', 'get-pip.py', '.ssh', '.gnupg',
-    'packages.txt', 'config', 'crontab']
+exclude = [
+    "*.sw*",
+    ".git",
+    "*.un~",
+    "install-dotfiles.py",
+    ".gitmodules",
+    "Makefile",
+    "build",
+    "requirements.txt",
+    ".config",
+    "get-pip.py",
+    ".ssh",
+    ".gnupg",
+    "packages.txt",
+    "config",
+    "crontab",
+]
 
-for e in os.scandir('.'):
+for e in os.scandir("."):
     if not any(fnmatch.fnmatch(e.name, p) for p in exclude):
         create_symlink(e.name)
 
-for configdir in ('.config', '.ssh', '.gnupg'):
+for configdir in (".config", ".ssh", ".gnupg"):
     for e in os.scandir(configdir):
         if e.is_dir():
             continue
