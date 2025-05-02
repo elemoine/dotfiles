46ac0717018a6720161cfdf391235b3293097d5f
Éric Lemoine <eric.lemoine@getalma.eu>
2025-05-02 23:42:48 +0200

    nvim: add keymap for lsp_workspace_symbols

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -8,6 +8,8 @@ vim.api.nvim_set_keymap("v", "gq", "<Esc>", { noremap = true, silent = true })
 -- leader+space to clear out search
 vim.api.nvim_set_keymap("n", "<Leader><Space>", ":noh<CR>", { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap("n", "<Leader>fS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", { desc = "LSP workspace symbols"})
+
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 vim.opt.textwidth = 80
