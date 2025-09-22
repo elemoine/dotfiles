dc344c9cf744d1b0ebdac2c05ac6301833e1cc0b
Éric Lemoine <eric.lemoine@getalma.eu>
2025-09-22 13:37:32 +0200

    nvim: add custom keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<Leader><Leader>", "<C-w><C-w>", { noremap = true, silent = true })
+
+vim.keymap.set("i", "gq", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "gq", "<Esc>", { noremap = true, silent = true })
+
+-- leader+space to clear out search
+vim.keymap.set("n", "<Leader><Space>", ":noh<CR>", { noremap = true, silent = true })
+
+vim.keymap.set(
+  "n",
+  "<Leader>fS",
+  "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+  { desc = "LSP workspace symbols" }
+)
