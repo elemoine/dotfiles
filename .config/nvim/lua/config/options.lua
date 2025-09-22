087972e36f78dc6ac03a88c87dfb22fe5a0f3b86
Éric Lemoine <eric.lemoine@getalma.eu>
2025-09-22 13:34:58 +0200

    nvim: add custom options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.mapleader = ","
+
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "88"
